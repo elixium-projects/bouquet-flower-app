ac857ac5d192e38dd003edc95f0dcf0da59b42fc
aguradev <wahmayun.wm@gmail.com>
2025-01-03 23:25:10 +0800

    feat: setup trait image

---
+++ app/Traits/File.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
+
+trait File
+{
+    public function Upload($file, $path)
+    {
+        $imageName = time() . "-" . str()->random(50) . "-" . $file->extension();
+
+        FacadesStorage::disk("public")->putFileAs($path, $file, $imageName);
+
+        return $imageName;
+    }
+}
